11fc56450d36806d3f9ccf4b80d9d53cafd06256
Ed Sumerfield <esumerfd@bitbashers.org>
2011-02-06 15:57:09 -0500

    Compelte about blocks syntactic conversion

---
+++ about/AboutBlocks.groovy
@@ -0,0 +1,96 @@
+import org.bitbashers.koans.*
+
+class AboutBlocks extends Koans {
+  void method_with_block(closure) {
+    def result  = closure()
+    result
+  }
+
+  void test_methods_can_take_blocks() {
+    def yielded_result  = method_with_block { 1 + 2 }
+    assertEquals( __, yielded_result )
+  }
+
+  void test_blocks_can_be_defined_with_do_end_too() {
+    def yielded_result  = method_with_block { 1 + 2 }
+    assertEquals( __, yielded_result )
+  }
+
+  // ------------------------------------------------------------------
+
+  void method_with_block_arguments(closure) {
+     closure("Jim")
+  }
+
+  void test_blocks_can_take_arguments() {
+    def result  = method_with_block_arguments { argument ->
+      assertEquals( __, argument )
+    }
+  }
+
+  // ------------------------------------------------------------------
+
+  void many_yields(closure) {
+     closure("peanut")
+     closure("butter")
+     closure("and")
+     closure("jelly")
+  }
+
+  void test_methods_can_call_yield_many_times() {
+    def result  = []
+    many_yields { item -> result << item }
+    assertEquals( __, result )
+  }
+
+  // ------------------------------------------------------------------
+
+  void yield_tester() {
+    if (closure) {
+      closure()
+    } else {
+      "no_block"
+    }
+  }
+
+  void test_methods_can_see_if_they_have_been_called_with_a_block() {
+    assertEquals( __, yield_tester { "with_block" } )
+    assertEquals( __, yield_tester )
+  }
+
+  // ------------------------------------------------------------------
+
+  void test_block_can_affect_variables_in_the_code_where_they_are_created() {
+    def value  = "initial_value"
+    method_with_block { value = "modified_in_a_block" }
+    assertEquals( __, value )
+  }
+
+  void test_blocks_can_be_assigned_to_variables_and_called_explicitly() {
+    def add_one  = { n -> n + 1 }
+    assertEquals( __, add_one.call(10) )
+
+    // Alternative calling sequence
+    assertEquals( __, add_one[10] )
+  }
+
+  void test_stand_alone_blocks_can_be_passed_to_methods_expecting_blocks() {
+    def make_upper  = { n -> n.upcase }
+    def result  = method_with_block_arguments(make_upper)
+    assertEquals( __, result )
+  }
+
+  // ------------------------------------------------------------------
+
+  void method_with_explicit_block(block) {
+    block.call(10)
+  }
+
+  void test_methods_can_take_an_explicit_block_argument() {
+    assertEquals( __, method_with_explicit_block { n -> n * 2 } )
+
+    def add_one  = { n -> n + 1 }
+    assertEquals( __, method_with_explicit_block(add_one) )
+  }
+
+ }
--- org/bitbashers/koans/RubyToGroovy.groovy
+++ org/bitbashers/koans/RubyToGroovy.groovy
@@ -6,58 +6,34 @@ new File(rubyKoansDir).eachFileMatch(~/${aboutFile}\.rb/) { ruby ->
 
     System.err.println "Converting Ruby Koan ${ruby}"
     System.err.println "NOT A COMPLETE CONVERSION. Follow the following steps to complete the job manually"
+    System.err.println "- Anything to do with block_given?, yield or block arguments"
 
     def context = [:]
     context["initialized"] = []
 
     ruby.eachLine { rubyLine ->
 
-        def variables = ["empty_array", "array", "shifted_value"]
+        def variables = [
+            "empty_array", "array", "shifted_value", "expected_value", "actual_value",
+            "result", "yielded_result", "value", "add_one", "make_upper"
+        ]
 
         def match = [:]
-        match << [/#.*env ruby/:         { line, regex -> } ]
-        match << [/#.*ruby/:             { line, regex -> } ]
+        match << [/#.*env ruby/:         { line, regex -> "" } ]
+        match << [/#.*ruby/:             { line, regex -> "" } ]
+        match << [/#(.*)$/:              { line, regex -> line.replaceAll(regex, '//$1') } ]
         match << [/edgecase/:            { line, regex -> "import org.bitbashers.koans.*" } ]
-        match << [/^.*end.*$/:           { line, regex -> line.replaceAll("end", "}") } ]
 
-        // FIX CLASS DECLARATION
-        match << [/^class /:             { line, regex -> line.replace(" < EdgeCase::Koan", " extends Koans {") } ]
-
-        // CORRECT METHOD SIGNATURE AND START BRACE COUNTING
-        match << [/def (.*)/:            { line, regex -> 
-            context["start_of_method"] = true
-            context["open_brace"] = 0
-            context["initialized"] = []
-            line.replaceAll(regex, 'void $1() {')
-          }
-        ]
-
-        // CONVERT TEST UNIT ASSERT NAMES
-        match << [/assert_equal *(.*) */:{ line, regex -> line.replaceAll(regex, 'assertEquals( $1 )') } ]
-        match << [/assert_not_equal *(.*) */:{ line, regex -> line.replaceAll(regex, 'assertNotEquals( $1 )') } ]
-
-        // CONVERT UNSUPPORTED CLASS
-        match << [/Array.new/:           { line, regex -> line.replaceAll(regex, '[]') } ]
-
-        // USE GROOVY NEW INSTANCE SYNTAX
-        match << [/\.new/:               { line, regex -> line.replaceAll(/ ([^ .]+)\.new/, ' new $1()') } ]
-
-        // CHECK FOR VARIABLES AND INITIALIZE THEM IF NECESSARY
-        match << [/^.*$/:                { line, regex -> 
-            def name = variables.find { name ->
-                def foundName = null
-                if (line.contains(name) && !context["initialized"].contains(name) && context["open_brace"] > 0) {
-                    foundName = name
-                }
-                foundName
-            }
-            if (name) {
-                context["initialized"] << name
-                line = line.replaceAll(" (${name}) ", ' def $1 ')
-            }
-            line
-          } 
-        ]
+        // CLOSURES WORK DIFFERENTY. LAMBDA IS UNNECESSARY, AMPERSAND IS DIFFERNT AND yeild IS NOT SUPPORTED
+        match << [/ do /:                { line, regex -> line.replaceAll(regex, " { ") } ]
+        match << [/\|([^|]+)\|/:         { line, regex -> line.replaceAll(regex, '$1 ->') } ]
+        match << [/^end| end.*$/:        { line, regex -> line.replaceAll(regex, ' }') } ]
+        match << [/lambda */:            { line, regex -> line.replaceAll(regex, "") } ]
+        match << [/&([a-zA-Z_])/:        { line, regex -> line.replaceAll(regex, '$1') } ]
+        match << [/ yield /:             { line, regex -> line.replaceAll(regex, ' closure() ') } ]
+        match << [/ yield$/:             { line, regex -> line.replaceAll(regex, ' closure()') } ]
+        match << [/yield\(/:             { line, regex -> line.replaceAll(regex, ' closure(') } ]
+        match << [/block_given\?/:       { line, regex -> line.replaceAll(regex, 'closure') } ]
 
         // COUNT OPEN BRACES
         match << ['\\{': { line, regex -> 
@@ -78,13 +54,61 @@ new File(rubyKoansDir).eachFileMatch(~/${aboutFile}\.rb/) { ruby ->
           }
         ]
 
+        // IF ELSE END BLOCKS
+        match << [/if (.*)/:             { line, regex -> line.replaceAll(regex, 'if ($1) {') } ]
+        match << [/ elsif (.*)/:              { line, regex -> line.replaceAll(regex, ' } else if ($1) {') } ]
+        match << [/ else/:               { line, regex -> line.replaceAll(regex, ' } else {') } ]
+
+        // FIX CLASS DECLARATION
+        match << [/^class /:             { line, regex -> line.replace(" < EdgeCase::Koan", " extends Koans {") } ]
+
+        // CORRECT METHOD SIGNATURE AND START BRACE COUNTING
+        match << [/def (.*)/:            { line, regex -> 
+            context["start_of_method"] = true
+            context["open_brace"] = 0
+            context["initialized"] = []
+            line.replaceAll(regex, 'void $1() {')
+          }
+        ]
+
+        // CONVERT TEST UNIT ASSERT NAMES
+        match << [/ assert_equal *(.*) */:{ line, regex -> line.replaceAll(regex, ' assertEquals( $1 )') } ]
+        match << [/ assert_not_equal *(.*) */:{ line, regex -> line.replaceAll(regex, ' assertNotEquals( $1 )') } ]
+
+        // CONVERT UNSUPPORTED CLASS
+        match << [/Array.new/:           { line, regex -> line.replaceAll(regex, '[]') } ]
+
+        // USE GROOVY NEW INSTANCE SYNTAX
+        match << [/\.new/:               { line, regex -> line.replaceAll(/ ([^ .]+)\.new/, ' new $1()') } ]
+
+        // CHECK FOR VARIABLES AND INITIALIZE THEM IF NECESSARY
+        match << [/^.*$/:                { line, regex -> 
+            def name = variables.find { name ->
+                def foundName = null
+                if (line =~ /^(\s+)(${name} )/ && !context["initialized"].contains(name)) {
+                    foundName = name
+                }
+                foundName
+            }
+            if (name) {
+                context["initialized"] << name
+                line = line.replaceAll(/^(\s+)(${name} )/, '$1def $2 ')
+            }
+            line
+          } 
+        ]
+
         // CONVERT SYMBOLS TO STRINGS
         match << [/:([a-zA-Z_]+)/: { line, regex -> line.replaceAll(regex, '"$1"') } ]
 
         // NOTE SUPPORTED BY GROOVY
         match << [/\.\.\./: { line, regex -> "//$line" } ]
 
-        // match << [//: { line, regex -> }]
+        // SPECIAL CASE TRANSLATIONS SPECIFIC TO RUBY_KOANS
+        match << [/(many_yields\()/:                 { line, regex -> line.replaceAll(regex, '$1closure') } ]
+        match << [/(method_with_block\()/:           { line, regex -> line.replaceAll(regex, '$1closure') } ]
+        match << [/(void method_with_block_arguments\()/: { line, regex -> line.replaceAll(regex, '$1closure') } ]
+        match << [/(method_with_explicit_block)\(block\)\(/: { line, regex -> line.replaceAll(regex, '$1(block') } ]
 
         def groovyLine = rubyLine
         System.err.println("## ${groovyLine}")
@@ -99,7 +123,7 @@ new File(rubyKoansDir).eachFileMatch(~/${aboutFile}\.rb/) { ruby ->
                 }
             }
             catch (java.util.regex.PatternSyntaxException e) {
-                println "ERROR: ${pattern} for line ${groovyLine}"
+                System.err.println "ERROR: '${e.message}': ${pattern} for line ${groovyLine}"
             }
         }
 

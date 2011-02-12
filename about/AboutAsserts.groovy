486ef78a0c277498877fc4e36d5a9a80c436af7b
Ed Sumerfield <esumerfd@bitbashers.org>
2011-02-12 17:53:57 -0500

    Add answers for asserts and blocks

--- about/AboutAsserts.groovy
+++ about/AboutAsserts.groovy
@@ -1,25 +1,22 @@
-
-
-
 import org.bitbashers.koans.*
 
 class AboutAsserts extends Koans {
 
   // We shall contemplate truth by testing reality, via asserts.
   void test_assert_truth() {
-    assert false                // This should be true
+    assert true                // This should be true
   }
 
   // Enlightenment may be more easily achieved with appropriate
   // messages.
   void test_assert_with_message() {
-    assert false, "This should be true -- Please fix this"
+    assert true, "This should be true -- Please fix this"
   }
 
   // To understand reality, we must compare our expectations against
   // reality.
   void test_assert_equality() {
-    def expected_value = __
+    def expected_value = 2
     def actual_value = 1 + 1
 
     assert expected_value == actual_value
@@ -27,7 +24,7 @@ class AboutAsserts extends Koans {
 
   // Some ways of asserting equality are better than others.
   void test_a_better_way_of_asserting_equality() {
-    def expected_value = __
+    def expected_value = 2
     def actual_value = 1 + 1
 
     assertEquals( expected_value, actual_value )
@@ -35,6 +32,6 @@ class AboutAsserts extends Koans {
 
   // Sometimes we will ask you to fill in the values
   void test_fill_in_values() {
-    assertEquals( __, 1 + 1 )
+    assertEquals( 2, 1 + 1 )
   }
 }
--- about/AboutBlocks.groovy
+++ about/AboutBlocks.groovy
@@ -1,30 +1,30 @@
 import org.bitbashers.koans.*
 
 class AboutBlocks extends Koans {
-  void method_with_block(closure) {
+  def method_with_block(closure) {
     def result  = closure()
     result
   }
 
   void test_methods_can_take_blocks() {
     def yielded_result  = method_with_block { 1 + 2 }
-    assertEquals( __, yielded_result )
+    assertEquals( 3, yielded_result )
   }
 
-  void test_blocks_can_be_defined_with_do_end_too() {
-    def yielded_result  = method_with_block { 1 + 2 }
-    assertEquals( __, yielded_result )
-  }
+//   void test_blocks_can_be_defined_with_do_end_too() {
+//     def yielded_result  = method_with_block do 1 + 2 end
+//     assertEquals( __, yielded_result )
+//   }
 
   // ------------------------------------------------------------------
 
-  void method_with_block_arguments(closure) {
+  def method_with_block_arguments(closure) {
      closure("Jim")
   }
 
   void test_blocks_can_take_arguments() {
     def result  = method_with_block_arguments { argument ->
-      assertEquals( __, argument )
+      assertEquals( "Jim", argument )
     }
   }
 
@@ -40,12 +40,12 @@ class AboutBlocks extends Koans {
   void test_methods_can_call_yield_many_times() {
     def result  = []
     many_yields { item -> result << item }
-    assertEquals( __, result )
+    assertEquals( ["peanut", "butter", "and", "jelly"], result )
   }
 
   // ------------------------------------------------------------------
 
-  void yield_tester() {
+  def yield_tester(closure = null) {
     if (closure) {
       closure()
     } else {
@@ -54,8 +54,8 @@ class AboutBlocks extends Koans {
   }
 
   void test_methods_can_see_if_they_have_been_called_with_a_block() {
-    assertEquals( __, yield_tester { "with_block" } )
-    assertEquals( __, yield_tester )
+    assertEquals( "with_block", yield_tester { "with_block" } )
+    assertEquals( "no_block", yield_tester(null) )
   }
 
   // ------------------------------------------------------------------
@@ -63,34 +63,33 @@ class AboutBlocks extends Koans {
   void test_block_can_affect_variables_in_the_code_where_they_are_created() {
     def value  = "initial_value"
     method_with_block { value = "modified_in_a_block" }
-    assertEquals( __, value )
+    assertEquals( "modified_in_a_block", value )
   }
 
   void test_blocks_can_be_assigned_to_variables_and_called_explicitly() {
     def add_one  = { n -> n + 1 }
-    assertEquals( __, add_one.call(10) )
+    assertEquals( 11, add_one.call(10) )
 
     // Alternative calling sequence
-    assertEquals( __, add_one[10] )
+    assertEquals( 11, add_one(10) )
   }
 
   void test_stand_alone_blocks_can_be_passed_to_methods_expecting_blocks() {
-    def make_upper  = { n -> n.upcase }
+    def make_upper  = { n -> n.toUpperCase() }
     def result  = method_with_block_arguments(make_upper)
-    assertEquals( __, result )
+    assertEquals( "JIM", result )
   }
 
   // ------------------------------------------------------------------
 
-  void method_with_explicit_block(block) {
+  def method_with_explicit_block(block) {
     block.call(10)
   }
 
   void test_methods_can_take_an_explicit_block_argument() {
-    assertEquals( __, method_with_explicit_block { n -> n * 2 } )
+    assertEquals( 20, method_with_explicit_block { n -> n * 2 } )
 
     def add_one  = { n -> n + 1 }
-    assertEquals( __, method_with_explicit_block(add_one) )
+    assertEquals( 11, method_with_explicit_block(add_one) )
   }
-
  }

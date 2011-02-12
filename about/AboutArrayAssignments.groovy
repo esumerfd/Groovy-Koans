a882813b6775b0db88f5d602b38c1eb8e5c282e9
Ed Sumerfield <esumerfd@bitbashers.org>
2011-02-12 15:03:08 -0500

    Add the answers to the first two koans

--- about/AboutArrayAssignments.groovy
+++ about/AboutArrayAssignments.groovy
@@ -4,19 +4,19 @@ class ArrayAssignmentKoans extends Koans {
 
   void test_non_parallel_assignment() {
     def names = ["John", "Smith"]
-    assertEquals __, names
+    assertEquals ["John", "Smith"], names
   }
 
   void test_parallel_assignments() {
     def first_name, last_name = ["John", "Smith"]
-    assertEquals __, first_name
-    assertEquals __, last_name
+    assertEquals "John", first_name
+    assertEquals "Smith", last_name
   }
 
   void test_parallel_assignments_with_extra_values() {
     def first_name, last_name = ["John", "Smith", "III"]
-    assertEquals __, first_name
-    assertEquals __, last_name
+    assertEquals "John", first_name
+    assertEquals "Smith", last_name
   }
 
 //   void test_parallel_assignments_with_splat_operator() {
@@ -27,14 +27,14 @@ class ArrayAssignmentKoans extends Koans {
 
   void test_parallel_assignments_with_too_few_variables() {
     def first_name, last_name = ["Cher"]
-    assertEquals __, first_name
-    assertEquals __, last_name
+    assertEquals "Cher", first_name
+    assertEquals null, last_name
   }
 
   void test_parallel_assignments_with_subarrays() {
     def first_name, last_name = [["Willie", "Rae"], "Johnson"]
-    assertEquals __, first_name
-    assertEquals __, last_name
+    assertEquals ["Willie", "Rae"], first_name
+    assertEquals "Johnson", last_name
   }
 
 //   void test_parallel_assignment_with_one_variable() {
@@ -46,7 +46,7 @@ class ArrayAssignmentKoans extends Koans {
     def first_name = "Roy"
     def last_name = "Rob"
     (first_name, last_name) = [last_name, first_name]
-    assertEquals __, first_name
-    assertEquals __, last_name
+    assertEquals "Rob", first_name
+    assertEquals "Roy", last_name
   }
 }
--- about/AboutArrays.groovy
+++ about/AboutArrays.groovy
@@ -3,8 +3,8 @@ import org.bitbashers.koans.*
 class AboutArrays extends Koans {
   void test_creating_arrays() {
     def empty_array = []
-    assertEquals( __, empty_array.class )
-    assertEquals( __, empty_array.size )
+    assertEquals( ArrayList, empty_array.class )
+    assertEquals( 0, empty_array.size )
   }
 
   void test_array_literals() {
@@ -15,70 +15,70 @@ class AboutArrays extends Koans {
     assertEquals( [1], array )
 
     array[1] = 2
-    assertEquals( [1, __], array )
+    assertEquals( [1, 2], array )
 
     array << 333
-    assertEquals( __, array )
+    assertEquals( [1,2,333], array )
   }
 
   void test_accessing_array_elements() {
     def array = ["peanut", "butter", "and", "jelly"]
 
-    assertEquals( __, array[0] )
-    assertEquals( __, array.first )
-    assertEquals( __, array[3] )
-    assertEquals( __, array.last )
-    assertEquals( __, array[-1] )
-    assertEquals( __, array[-3] )
+    assertEquals( "peanut", array[0] )
+    assertEquals( "peanut", array.first() )
+    assertEquals( "jelly", array[3] )
+    assertEquals( "jelly", array.last())
+    assertEquals( "jelly", array[-1] )
+    assertEquals( "butter", array[-3] )
   }
 
   void test_slicing_arrays() {
     def array = ["peanut", "butter", "and", "jelly"]
 
-    assertEquals( __, array[0,1] )
-    assertEquals( __, array[0,2] )
-    assertEquals( __, array[2,2] )
-    assertEquals( __, array[2,20] )
-    assertEquals( __, array[4,0] )
-    assertEquals( __, array[4,100] )
-    assertEquals( __, array[5,0] )
+    assertEquals( ["peanut","butter"], array[0,1] )
+    assertEquals( ["peanut","and"], array[0,2] )
+    assertEquals( ["and","and"], array[2,2] )
+    assertEquals( ["and",null], array[2,20] )
+    assertEquals( [null,"peanut"], array[4,0] )
+    assertEquals( [null,null], array[4,100] )
+    assertEquals( [null, "peanut"], array[5,0] )
   }
 
   void test_arrays_and_ranges() {
-    assertEquals( __, (1..5).class )
-    assertNotEquals( [1,2,3,4,5], (1..5) )
-    assertEquals( __, (1..5).to_a )
-//    assertEquals( __, (1...5).to_a )
+    assertEquals( IntRange, (1..5).class )
+    assertEquals( [1,2,3,4,5], (1..5) )
+//    assertEquals( __, (1..5).to_a )
+    assertEquals( [1,2,3,4], (1..<5) )
   }
 
   void test_slicing_with_ranges() {
     def array = ["peanut", "butter", "and", "jelly"]
 
-    assertEquals( __, array[0..2] )
-//    assertEquals( __, array[0...2] )
-    assertEquals( __, array[2..-1] )
+    assertEquals( ["peanut", "butter", "and"], array[0..2] )
+    assertEquals( ["peanut", "butter"], array[0..<2] )
+    assertEquals( ["and", "jelly"], array[2..-1] )
   }
 
   void test_pushing_and_popping_arrays() {
     def array = [1,2]
     array.push("last")
 
-    assertEquals( __, array )
+    assertEquals( [1,2,"last"], array )
 
-    popped_value = array.pop
-    assertEquals( __, popped_value )
-    assertEquals( __, array )
+    def popped_value = array.pop()
+    assertEquals( "last", popped_value )
+    assertEquals( [1,2], array )
   }
 
-  void test_shifting_arrays() {
-    def array = [1,2]
-    array.unshift("first")
+//   void test_shifting_arrays() {
+//     def array = [1,2]
+//     array.unshift("first")
 
-    assertEquals( __, array )
+//     assertEquals( __, array )
 
-    def shifted_value = array.shift
-    assertEquals( __, shifted_value )
-    assertEquals( __, array )
-  }
+//     def shifted_value = array.shift
+//     assertEquals( __, shifted_value )
+//     assertEquals( __, array )
+//   }
 
 }
